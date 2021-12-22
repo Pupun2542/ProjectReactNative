6f9baa374a4bd688c50ed07f302ad74c6871ff63
Pupun2542 <ppupun2542@gmail.com>
2021-12-22 11:55:16 +0700

    WebView and Stack Navigation

---
+++ rnproject/screens/AboutScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const AboutScreen = () => {
+    return (
+        <View style>
+            <Text>เกี่ยวกับเรา</Text>
+        </View>
+    )
+}
+
+export default AboutScreen
+
+const styles = StyleSheet.create({})
